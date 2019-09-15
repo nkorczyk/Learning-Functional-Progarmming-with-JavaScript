9fce46937ed8bcc37214ce9ed44a26f6681a1131
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-15 18:59:32 +0200

    Passing functions as arguments

--- 02_03/Begin/01_doif.js
+++ 02_03/Begin/01_doif.js
@@ -1,22 +1,24 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 03, Exercise 01
 
+function doIf(condition, func) {
+  if (condition) {
+    func();
+  }
+}
+
 const x = 1
 
-if (x === 1) {
-  sayXis1()
+doIf(x === 1, sayXis1);
+doIf(x === "Bananas", sayXisBananas);
+doIf(x < 10 && x > 0, sayXisBetween0And10);
+
+function sayXis1() {
+  console.log("x is equal to 1")
 }
-
-if (x === "Bananas") {
-  sayXisBananas()
+function sayXisBananas() {
+  console.log("x is equal to 'Bananas'")
 }
-
-if (x < 10 && x > 0) {
-  sayXisBetween0And10()
+function sayXisBetween0And10() {
+  console.log("x is between 0 and 10")
 }
-
-function sayXis1() { console.log("x is equal to 1") }
-
-function sayXisBananas() { console.log("x is equal to 'Bananas'") }
-
-function sayXisBetween0And10() { console.log("x is between 0 and 10") }
--- 02_03/Begin/02_withArguments.js
+++ 02_03/Begin/02_withArguments.js
@@ -1,5 +1,11 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 03, Exercise 02
+function callWith2Arguments(arg1, arg2, func) {
+  return func(arg1, arg2);
+}
+
+callWith2Arguments(1, 2, add);
+callWith2Arguments(9, 4, subtract);
 
 function add(x, y) {
   return x + y
