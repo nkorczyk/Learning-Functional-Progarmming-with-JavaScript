dbfaa2b552bb9be19103aefeb7aa7988c0e0bfc1
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-15 23:12:48 +0200

    Mapping

--- 03_02/Begin/01_simpleMap.js
+++ 03_02/Begin/01_simpleMap.js
@@ -1,10 +1,22 @@
 // Learning Functional Programming with Javascript
 // Chapter 03, Video 02, Exercise 01
 
-var numbers = [ 1, 2, 3, 4, 5 ]
-var numbersCubed = [ ]
+var _ = require('lodash');
 
-for(var i = 0; i < numbers.length; i++) {
-  var element = numbers[i]
-  numbersCubed.push(element * element * element)
-}
+// var numbers = [1, 2, 3, 4, 5];
+// var numbersCubed = [];
+
+// for (var i = 0; i < numbers.length; i++) {
+//   var element = numbers[i];
+//   numbersCubed.push(element * element * element);
+// }
+
+// console.log("numbers", numbers);
+// console.log("numbersCubed", numbersCubed);
+
+var numbers = [1, 2, 3, 4, 5];
+var numbersCubed = _.map(numbers, function(element) {
+  return element * element * element ;
+});
+
+console.log("numbersCubed", numbersCubed);
