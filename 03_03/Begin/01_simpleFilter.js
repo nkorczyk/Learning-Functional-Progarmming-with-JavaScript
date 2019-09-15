58f8c31341ddfef746e381c62a431b732355df02
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-15 23:23:01 +0200

    Filtering

--- 03_03/Begin/01_simpleFilter.js
+++ 03_03/Begin/01_simpleFilter.js
@@ -1,12 +1,21 @@
 // Learning Functional Programming with Javascript
 // Chapter 03, Video 03, Exercise 01
 
-var numbers = [ 1, 2, 3, 4, 5 ]
-var evensFromNumbers = [ ]
+var _ = require('lodash');
 
-for (var i = 0; i < numbers.length; i++) {
-  var element = numbers[i]
-  if (element % 2 === 0) {
-    evensFromNumbers.push(element)
-  }
-}
+// var numbers = [ 1, 2, 3, 4, 5 ]
+// var evensFromNumbers = [ ]
+
+// for (var i = 0; i < numbers.length; i++) {
+//   var element = numbers[i]
+//   if (element % 2 === 0) {
+//     evensFromNumbers.push(element)
+//   }
+// }
+
+var numbers = [1, 2, 3, 4, 5]
+var evensFromNumbers = _.filter(numbers, function(element) {
+  return element % 2 === 0;
+});
+
+console.log("evensFromNumbers", evensFromNumbers);
--- 03_03/Begin/02_employeesFilter.js
+++ 03_03/Begin/02_employeesFilter.js
@@ -1,18 +1,24 @@
 // Learning Functional Programming with Javascript
 // Chapter 03, Video 03, Exercise 02
 
+var _ = require('lodash');
+
 var employees = [
-  { name: "John",  salary: 50000  },
-  { name: "Susan", salary: 60000  },
-  { name: "Greg",  salary: 100000 },
-  { name: "Mary",  salary: 120000 }
+  { name: "John", salary: 50000 },
+  { name: "Susan", salary: 60000 },
+  { name: "Greg", salary: 100000 },
+  { name: "Mary", salary: 120000 }
 ]
 
-var dueForARaise = [ ]
+var dueForARaise = _.filter(employees, function(employee) {
+  return employee.salary < 70000;
+});
 
-for (var i = 0; i < employees.length; i++) {
-  var employee = employees[i]
-  if (employee.salary < 70000) {
-    dueForARaise.push(employee)
-  }
-}
+console.log("dueForARaise", dueForARaise);
+
+// for (var i = 0; i < employees.length; i++) {
+//   var employee = employees[i]
+//   if (employee.salary < 70000) {
+//     dueForARaise.push(employee)
+//   }
+// }
