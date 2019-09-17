49ad58431041727b6e184155674a9e7a2147b9e8
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-17 18:16:50 +0200

    Every/Some

--- 03_04/Begin/01_containsEven.js
+++ 03_04/Begin/01_containsEven.js
@@ -1,22 +1,35 @@
 // Learning Functional Programming with Javascript
 // Chapter 03, Video 04, Exercise 01
+var _ = require('lodash');
 
-var numbers = [ 2, 4, 6, 8, 10, 12 ]
+var numbers = [3, 4, 6, 8, 10, 12];
 
-var arrayContainsEven = false
+// var arrayContainsEven = false;
 
-for (var i = 0; i < numbers.length; i++) {
-  var element = numbers[i]
-  if (element % 2 === 0) {
-    arrayContainsEven = true
-  }
-}
+// for (var i = 0; i < numbers.length; i++) {
+//   var element = numbers[i];
+//   if (element % 2 === 0) {
+//     arrayContainsEven = true;
+//   }
+// }
 
-var arrayIsAllEven = true
+// var arrayIsAllEven = true
 
-for (var i = 0; i < numbers.length; i++) {
-  var element = numbers[i]
-  if (!(element % 2 === 0)) {
-    arrayIsAllEven = false
-  }
-}
+// for (var i = 0; i < numbers.length; i++) {
+//   var element = numbers[i];
+//   if (!(element % 2 === 0)) {
+//     arrayIsAllEven = false;
+//   }
+// }
+
+var arrayContainsEven = _.some(numbers, function(element) {
+  return element % 2 === 0;
+});
+
+console.log('arrayContainsEven', arrayContainsEven);
+
+var arrayIsAllEven = _.every(numbers, function(element) {
+  return element % 2 === 0;
+})
+
+console.log('arrayIsAllEven', arrayIsAllEven);
