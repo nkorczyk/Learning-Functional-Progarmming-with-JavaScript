9ad22231d688da0888d053dae276b2111f490c39
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-17 18:52:26 +0200

    Reducing

--- 03_05/Begin/01_totalCost.js
+++ 03_05/Begin/01_totalCost.js
@@ -1,9 +1,16 @@
 // Learning Functional Programming with Javascript
 // Chapter 03, Video 05, Exercise 01
+var _ = require('lodash');
 
-var shoppingList = [
-  { name: "Eggs",    price: 4.99 },
-  { name: "Milk",    price: 3.99 },
+const shoppingList = [
+  { name: "Eggs", price: 4.99 },
+  { name: "Milk", price: 3.99 },
   { name: "Bananas", price: 2.79 },
-  { name: "Beer",    price: 6.99 }
-]
+  { name: "Beer", price: 6.99 }
+];
+
+const sum = _.reduce(shoppingList, function(acc, element) {
+  return acc + element.price;
+}, 0);
+
+console.log('sum', sum);
