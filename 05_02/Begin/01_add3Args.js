bec8e15f8154758a737d4bf4a6b1f553d1a11e77
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-17 19:54:12 +0200

    Partial application

--- 05_02/Begin/01_add3Args.js
+++ 05_02/Begin/01_add3Args.js
@@ -1,16 +1,16 @@
 // Learning Functional Programming with Javascript
 // Chapter 05, Video 02, Exercise 01
 
-function add(x, y) {
-  return x + y
+function add(x, y, z) {
+  return x + y + z;
 }
 
-function partiallyApply(func, x) {
-  return function(y) {
-    return func(x, y)
+function partiallyApply(func, x, y) {
+  return function (z) {
+    return func(x, y, z);
   }
 }
 
-var add5 = partiallyApply(add, 5)
+var add5 = partiallyApply(add, 5, 2);
 
-console.log(add5(2))
+console.log(add5(3));
