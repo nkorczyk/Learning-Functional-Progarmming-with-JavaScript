89e60fe4f708b1430b5db7e6972c55606f1d8a0a
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-17 19:41:44 +0200

    Callbacks

--- 04_02/Begin/01_setTimeout.js
+++ 04_02/Begin/01_setTimeout.js
@@ -1,2 +1,25 @@
 // Learning Functional Programming with Javascript
 // Chapter 04, Video 02, Exercise 01
+
+console.log('Before setTimeout');
+
+setTimeout(function() {
+  console.log('The function has been executed');
+}, 3000);
+
+console.log('After setTimeout');
+
+var x = 1;
+
+console.log('x is ' + x);
+
+setTimeout(function() {
+  x = 99;
+  console.log('x now is ' + x);
+  setTimeout(function() {
+    x = 43;
+    console.log('x now is ' + x);
+  }, 3000);
+}, 3000);
+
+console.log('x is ' + x);
